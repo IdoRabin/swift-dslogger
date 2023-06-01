056c71c21e8e6c1c9264cb425ff053d4e2894862
Ido Rabin <ido@sync.me>
2023-06-01 18:50:54 +0300

    Version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "DSLogger",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -24,5 +28,7 @@ let package = Package(
         .testTarget(
             name: "DSLoggerTests",
             dependencies: ["DSLogger"]),
-    ]
+    ],
+    
+    swiftLanguageVersions: [.v5]
 )
---
+++ Sources/DSLogger/Version.swift
@@ -0,0 +1,26 @@
+//  Version.swift
+
+import AppKit
+
+enum PreRelease: String {
+    case none = ""
+    case alpha = "alpha"
+    case beta = "beta"
+    case RC = "RC"
+}
+
+// https://semver.org/
+// Swift package PackageDescription also supports Sever2 Version struct defined, but we will be using ver 1.0.0
+
+// Hard coded app version:
+let DSLOGGER_NAME_STR : String = "DSLogger"
+
+// String fields allow only alphanumerics and a hyphen (-)
+let DSLOGGER_BUILD_NR : Int = 1
+let DSLOGGER_BUILD_VERSION = MNSemver (
+    major: 0,
+    minor: 1,
+    patch: 0,
+    prerelease: "\(PreRelease.alpha.rawValue)",
+    metadata: [String(format: "%04X", DSLOGGER_BUILD_NR)]
+)
